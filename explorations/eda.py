0d7ea7e0a5361786eeff68f7d86da04dd08841c2
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-05-25 13:19:28 +0000

    feat: ingest raw data to bronze

--- explorations/eda.py
+++ explorations/eda.py
@@ -1,2 +1,88 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "2"
+# ///
+# MAGIC %md
+# MAGIC # EDA on ultra-marathon running
 
+# COMMAND ----------
+
+df = spark.read.csv("/Volumes/marathos/default/raw/TWO_CENTURIES_OF_UM_RACES.csv", header=True, inferSchema=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Number of rows
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Number of columns
+
+# COMMAND ----------
+
+len(df.columns)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Check schema
+
+# COMMAND ----------
+
+df.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Descriptive summary
+
+# COMMAND ----------
+
+df.describe()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Null count per column
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+df.select([
+    F.sum(F.col(c).isNull().cast("int")).alias(c)
+    for c in df.columns
+]).display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Number of unique events
+
+# COMMAND ----------
+
+df.select(F.col("Event name")).distinct().count()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Age distrubution of runners
+
+# COMMAND ----------
+
+df.groupBy(F.col("Athlete year of birth")).count().orderBy("Athlete year of birth").display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Countries with most participants
+
+# COMMAND ----------
+
+df.groupBy(F.col("Athlete country")).count().orderBy("count", ascending=False).show()
