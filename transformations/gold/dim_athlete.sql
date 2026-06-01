2cf550999b7879fdfabbdac0ffe666f6d14cd2e5
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-06-01 08:35:13 +0000

    feat: genie dashboard
    Verification on genie dashboards result

--- transformations/gold/dim_athlete.sql
+++ transformations/gold/dim_athlete.sql
@@ -1,8 +1,11 @@
 CREATE MATERIALIZED VIEW marathos.gold.dim_athlete AS
-SELECT DISTINCT
+SELECT
   athlete_id,
-  athlete_gender,
-  athlete_club,
-  athlete_country,
-  athlete_year_of_birth
-FROM marathos.silver.races_clean_obt
+  MAX(athlete_gender) AS athlete_gender,
+  MAX(athlete_club) AS athlete_club,
+  MAX(athlete_country) AS athlete_country,
+  MAX(athlete_year_of_birth) AS athlete_year_of_birth
+FROM
+  marathos.silver.races_clean_obt
+GROUP BY
+  athlete_id;
